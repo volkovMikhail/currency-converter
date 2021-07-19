3e278899ae6a2a5ae5cc024b626b7ca00fcb510f
Mikhail Volkov <volkov.work1@gmail.com>
2021-07-19 13:11:01 +0300

    add npm scripts

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require('path');
 const express = require("express");
 const router = require("./routes/apiRoutes");
 
@@ -8,10 +9,11 @@ const app = express();
 
 app.use("/api/", router);
 
+app.use('/',express.static(path.join(__dirname,'client','build')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+})
+
 app.listen(port, () => {
     console.log(`server started on port: ${port}`);
-});
-
-app.get("*", (req, res) => {
-    res.status(404).end("404");
 });
